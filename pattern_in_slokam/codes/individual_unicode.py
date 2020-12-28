d0a3d7e084a08e9fa000bbef3d990524028b5357
chakki1234 <chakkirala.a@gmail.com>
2020-12-29 01:45:22 +0530

    added latex doc and optimized the code

--- pattern_in_slokam/codes/individual_unicode.py
+++ pattern_in_slokam/codes/individual_unicode.py
@@ -1,14 +1,21 @@
-sloka = open('aigiri_test2.txt', 'r')
-sloka_txt = sloka.read()
-sloka.close()
+def read(file_name):
+  sloka = open(file_name, 'r')
+  sloka_txt = sloka.read()
+  sloka.close()
+  return sloka_txt
 
-words = sloka_txt.split()
-uni_file = open('unicode_test2.txt', 'w')
+def split_to_words(txt, out_file_name):
+  words = txt.split()
+  uni_file = open(out_file_name, 'w')
 
-for i in words:
-    if(i !='|' and i !='||' and not(i.isdigit())):
-        for j in i:
-            uni_file.write('U+' + hex(ord(j)).replace('x', ''))
-            uni_file.write('  ')
+  for i in words:
+      if(i !='|' and i !='||' and not(i.isdigit())):
+          for j in i:
+              uni_file.write('U+' + hex(ord(j)).replace('x', ''))
+              uni_file.write('  ')
 
-uni_file.close()
+  uni_file.close()
+
+split_to_words(read('aigiri_test1.txt'), 'unicode_test1.txt')
+split_to_words(read('aigiri_test2.txt'), 'unicode_test2.txt')
+split_to_words(read('aigiri.txt'), 'unicode.txt')
